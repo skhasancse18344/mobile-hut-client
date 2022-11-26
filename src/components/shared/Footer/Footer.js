c8bf4b04b0829937bf8f468bc0614861f003ebb9
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-11-26 19:19:27 +0600

    Nav bar

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="mt-10">This is Footer</div>;
+  return (
+    <div className="mt-10 w-full bg-black py-6 text-center text-white  ">
+      <p>Copyright &copy; 2022 || Kamrul Hasan</p>
+    </div>
+  );
 };
 
 export default Footer;
