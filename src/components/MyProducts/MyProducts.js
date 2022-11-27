0975000859883708359aefb7d219ebc4602d1133
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2022-11-27 23:19:21 +0600

    My Product

--- src/components/MyProducts/MyProducts.js
+++ src/components/MyProducts/MyProducts.js
@@ -1,4 +1,4 @@
-gimport { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import React, { useContext } from "react";
 import { AuthContext } from "../Contexts/AuthProvider";
 import MyProductCard from "./MyProductCard";
