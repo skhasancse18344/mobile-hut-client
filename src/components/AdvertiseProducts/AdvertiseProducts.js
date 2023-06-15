3c56bdd9127333a19464c632aa294a9eca59bac2
skhasancse18344 <kamrul.hasan.cse18344@gmail.com>
2023-06-15 14:33:08 +0600

    Some Changes

--- src/components/AdvertiseProducts/AdvertiseProducts.js
+++ src/components/AdvertiseProducts/AdvertiseProducts.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import bgimage from "../../assets/mobile_phones.jpg";
+
 import AdvertiseProductCard from "./AdvertiseProductCard";
 
 const AdvertiseProducts = () => {
@@ -21,18 +21,7 @@ const AdvertiseProducts = () => {
       ) : (
         <div>
           {" "}
-<<<<<<< HEAD
           <h1 className="text-4xl text-center font-bold my-20 py-8 text-white  bg-slate-900">
-=======
-          <h1
-            className="text-4xl text-center font-bold my-20 py-16 text-white rounded-md"
-            style={{
-              backgroundImage: `url(${bgimage})`,
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-            }}
-          >
->>>>>>> 94971d787d75595e1410d15142f5b86fd235819b
             Advertised Product
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-20">
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -15,15 +15,11 @@ const Banner = () => {
               Welcome To Mobile Hut,We Only Provide Best And Authentic Used
               Mobile.
             </p>
-<<<<<<< HEAD
             <div className="w-full flex   mt-6">
               <button className="btn btn-primary mx-auto text-white">
                 Get Started
               </button>
             </div>
-=======
-            <button className="btn btn-primary text-white">Get Started</button>
->>>>>>> 94971d787d75595e1410d15142f5b86fd235819b
           </div>
         </div>
       </div>
